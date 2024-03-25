3e61fe95ccf48252c2723b40f817489e8f8dccf6
Kenneth Seekins <kseekins@hawaii.edu>
2024-03-24 23:33:14 -0700

    round 2
    1st: DNF
    2nd: DNF

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Col, Container, Image, Row } from 'react-bootstrap';
+import { FileEarmarkTextFill, PeopleFill } from 'react-bootstrap-icons';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+        <PeopleFill width="100px" />
+        <h1>Multiple Users </h1>
+
       </Col>
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
+      <Col xs={4} className="d-flex flex-column justify-content-center">
+        <FileEarmarkTextFill />
         <p>Now get to work and modify this app!</p>
       </Col>
+
+      <Col xs={4} />
 
     </Row>
   </Container>
