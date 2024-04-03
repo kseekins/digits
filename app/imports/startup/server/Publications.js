002a73257746eb4db304228d0867becbcaa13bde
Kenneth Seekins <kseekins@hawaii.edu>
2024-04-02 18:29:18 -1000

    2ns round
    2nd round dnf

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
 import { Contacts } from '../../api/contact/contacts';
-import { Notes } from '../../api/note/notes';
+import { Notes } from '../../api/note/Notes';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
@@ -25,7 +25,7 @@ Meteor.publish(Contacts.userPublicationName, function () {
 Meteor.publish(Notes.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
-    return Contacts.collection.find({ owner: username });
+    return Notes.collection.find({ owner: username });
   }
   return this.ready();
 });
@@ -42,6 +42,13 @@ Meteor.publish(Stuffs.adminPublicationName, function () {
 Meteor.publish(Contacts.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Contacts.collection.find();
+  }
+  return this.ready();
+});
+
+Meteor.publish(Notes.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Notes.collection.find();
   }
   return this.ready();
 });
--- app/imports/ui/components/Note.jsx
+++ app/imports/ui/components/Note.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { ListGroup } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const Contact = ({ contact }) => (
-  <Card className="h-100">
-    <Card.Header>
-      <Image src={contact.image} width={75} />
-      <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
-      <Card.Subtitle>{contact.address}</Card.Subtitle>
-    </Card.Header>
-    <Card.Body>
-      <Card.Text>{contact.description}</Card.Text>
-      <Link to={`/edit/${contact._id}`}>Edit</Link>
-    </Card.Body>
-  </Card>
+const Note = ({ note }) => (
+  <ListGroup.Item>
+    <p className="fw-lighter">{note.createdAt.toLocaleDateString('en-US')}</p>
+    <p>{note.note}</p>
+  </ListGroup.Item>
 );
 
 // Require a document to be passed to this component.
-Contact.propTypes = {
-  contact: PropTypes.shape({
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
-    address: PropTypes.string,
-    image: PropTypes.string,
-    description: PropTypes.string,
+Note.propTypes = {
+  note: PropTypes.shape({
+    note: PropTypes.string,
+    contactId: PropTypes.string,
+    owner: PropTypes.string,
+    createdAt: PropTypes.instanceOf(Date),
     _id: PropTypes.string,
   }).isRequired,
 };
 
-export default Contact;
+export default Note;
